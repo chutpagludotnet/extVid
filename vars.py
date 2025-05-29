5dcd874313140c69f08a3a28da1df24adb32620a
chutpagludotnet <cluster@chutpaglu.net>
2025-05-30 00:54:49 +0300

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,12 @@
+#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+# Add your details here and then deploy by clicking on HEROKU Deploy button
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "21311268"))
+API_HASH = environ.get("API_HASH", "af66e56cee1a95ddf34b2b5d1e422b14")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8076392409:AAHdWYDiQ695szUAuAuiCXsibEhNY0l8yno")
+OWNER = int(environ.get("OWNER", "7527795504"))
+CREDIT = "★彡[ᴍᴇᴅᴜꜱᴀxᴅ]彡★"
+#WEBHOOK = True  # Don't change this
+#PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
