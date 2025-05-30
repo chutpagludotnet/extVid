184e4093f140df338ce68fd17ed4b9208908f8fa
chutpagludotnet <cluster@chutpaglu.net>
2025-05-30 06:34:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,20 +12,20 @@ def hello_world():
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png">
-    <titleSudoR2spr Repository</title>
-	<link rel="icon" type="image/x-icon" href="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png">
+    <link rel="stylesheet" type="text/css" href="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png">
+    <title>MedusaXD Repository</title>
+    <link rel="icon" type="image/x-icon" href="https://tinypic.host/images/2025/02/07/DeWatermark.ai_1738952933236-1.png">
 
 </head>
 
 <body>
     <div class="container" style="bg-dark text-red text-center py-3 mt-5">
-        <a href="https://chutpaglu.net" class="card">
+        <a href="https://github.com/nikhilsaini098" class="card">
             <p>
               />▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄<br 
-	      />██░▄▄▄░█░▄▄▀█▄░▄██░▀██░█▄░▄██<br 
+          />██░▄▄▄░█░▄▄▀█▄░▄██░▀██░█▄░▄██<br 
               />██▄▄▄▀▀█░▀▀░██░███░█░█░██░███<br 
-	      />██░▀▀▀░█░██░█▀░▀██░██▄░█▀░▀██<br 
+          />██░▀▀▀░█░██░█▀░▀██░██▄░█▀░▀██<br 
               />▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀<br />
                                              <br>
 
@@ -31,14 +33,14 @@ def hello_world():
             </p>
         </a>
     </div>
-	<br></br><br></br><br></br>
-	<footer class="bg-dark text-white text-center py-3 mt-5">
-	<center><img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/04/28/IMG_20250428_085026_585.jpg" width="40" height="40">
-        Powered By SAINI 
-		<img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/04/28/IMG_20250428_085026_585.jpg" width="40" height="40">
-		<div class="footer__copyright">
+    <br></br><br></br><br></br>
+    <footer class="bg-dark text-white text-center py-3 mt-5">
+    <center><img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/04/28/IMG_20250428_085026_585.jpg" width="40" height="40">
+        Powered By MEDUSA 
+        <img loading="lazy" class="object-none object-center" src="https://tinypic.host/images/2025/04/28/IMG_20250428_085026_585.jpg" width="40" height="40">
+        <div class="footer__copyright">
             <p class="footer__copyright-info">
-                © 2025 MedusaXD Video Downloader. All rights reserved.
+                © 2024 Video Downloader. All rights reserved.
             </p>
         </div>
     </footer></center>
@@ -47,6 +49,6 @@ def hello_world():
 </html>
 """
 
-
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
